c83ffc04c74476434b0fe4ee64f40090420031c9
albert <albert.greinoecker@gmail.com>
2024-12-02 11:08:57 +0100

    additional routes

--- ex_01_hello_world.py
+++ ex_01_hello_world.py
@@ -6,7 +6,23 @@ app = Flask(__name__) # Initialisierung der App
 
 @app.route('/')
 def home():
-    return 'Hello!!!'  #Dieser Text wird direkt auf der Website angezeigt
+    return '<b>Hello</b>!!!'  #Dieser Text wird direkt auf der Website angezeigt
+
+@app.route('/htl')
+def htl():
+    return '<b>Hello</b> HTL!!!'  #Dieser Text wird direkt auf der Website angezeigt
+
+@app.route('/name/<name>')
+def name(name):
+    return '<b>Hello</b> !!!' + name  #Dieser Text wird direkt auf der Website angezeigt
+
+@app.route('/add/<int:op1>/<int:op2>')
+def add(op1, op2):
+    return str(op1 + op2)  #Dieser Text wird direkt auf der Website angezeigt
+
+
+
+
 
 if __name__ == '__main__':
     app.run() #Startet den Server
